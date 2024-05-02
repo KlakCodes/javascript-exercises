d6b3f0ff21dd2f1b30915200d4e2348126f2f4f4
KlakCodes <harrisonjclark@hotmail.com>
2024-05-02 14:43:31 +0100

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const textArray = string.split("");
+    let revText = "";
+    
+    textArray.reverse();
 
+    for(let i =0; i < textArray.length; i++) {
+        revText = revText + textArray[i];
+    }
+
+    return revText;
 };
 
 // Do not edit below this line
