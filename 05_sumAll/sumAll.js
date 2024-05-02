e2196b65247a64f45eed5a2d18eb173b0af764eb
KlakCodes <harrisonjclark@hotmail.com>
2024-05-02 15:43:39 +0100

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let smallNum;
+    let bigNum;
+    let result = 0;
 
+    if((num1 > 0 && typeof num1 === "number") && (num2 > 0 && typeof num2 === "number")) {
+        if(num1 >= num2) {
+            bigNum = num1;
+            smallNum = num2;
+        } else if(num2 > num1) {
+            bigNum = num2;
+            smallNum = num1;
+        }
+    
+        for(let i = smallNum; i <= bigNum; i++) {
+            result = result + i;
+        }
+    
+        return result;
+    } else {
+        return result = "ERROR";
+    }
 };
 
 // Do not edit below this line
