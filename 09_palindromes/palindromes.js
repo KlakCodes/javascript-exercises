cda5f9b34ec784f3f78394b311880ba947f76489
KlakCodes <harrisonjclark@hotmail.com>
2024-06-13 14:53:19 +0100

    Completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let arr = str.toLowerCase().replace(/\s+/g, "").split('');
 
+    arr.forEach(char => {
+        if(char.length === 1 && !char.match(/^[a-z0-9]+$/i)) {
+            arr.splice(arr.indexOf(char), 1);
+        }
+    });
+
+    let string = arr.join('');
+    let revString = arr.reverse().join('');
+
+    return string === revString;
 };
 
 // Do not edit below this line
