3b4baec1eab5adf2fd7497409a2184d8b47772a3
KlakCodes <harrisonjclark@hotmail.com>
2024-06-14 12:48:58 +0100

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const today = new Date();
 
+    people.forEach((person) => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = today.getUTCFullYear();
+        }
+    });
+
+    let oldPeople = people.sort((a, b) => a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1);
+
+    return oldPeople[0];
 };
 
 // Do not edit below this line
