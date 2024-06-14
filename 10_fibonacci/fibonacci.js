9963c8830a9c2841283d278db080e10305d1eb14
KlakCodes <harrisonjclark@hotmail.com>
2024-06-14 10:47:04 +0100

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num >= 0) {
+        let fibSeq = [0, 1];
 
+        for (let i = 0; i <= num; i++) {
+            if(i > 1) {
+                fibSeq.push(fibSeq[i - 1] + fibSeq[i - 2]);
+            }
+        }
+
+        return fibSeq[num];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
